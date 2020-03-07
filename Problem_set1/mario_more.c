9d5e3753da04535526394d61c4e556497e2abae1
Maha Sabry <48715558+MahaSabry5@users.noreply.github.com>
2020-03-07 16:02:24 +0200

    Add files via upload

---
+++ Problem_set1/mario_more.c
@@ -0,0 +1,33 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int n ;
+    //get the height from the user between 1 and 8
+    do
+    {
+        n = get_int("Enter positive number: ");
+    }
+    while (n < 1 || n > 8);
+    // iterates the height and print spaces and hashes for the left part
+    for (int i = 0 ; i < n ; i++)
+    {
+        for (int k = 1 ; k < n - i ; k++)
+        {
+            printf("%c", ' ');
+        }
+        for (int j = 0 ; j < i + 1 ; j++)
+        {
+            printf("#");
+        }
+        printf("%s", "  ");
+        for (int m = 0 ; m < i+1 ; m++)
+        {
+            printf("#");
+        }
+
+        printf("\n");
+    
+    }
+}
